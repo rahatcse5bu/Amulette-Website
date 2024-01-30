b6230079c219b16402841bd082cb5829d6e18619
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-01-30 08:01:51 +0600

    some issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primaryColor:'#F65F19',
+				menuColor : 'rgba(37, 40, 43, 0.70)',
+				headingColor:'#25282B',
+			}
+		},
 	},
 	plugins: [],
 };
