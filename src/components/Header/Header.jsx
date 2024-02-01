b39ad07ce39905e481ff7990660f38c0b46c7ce4
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-02-01 19:37:11 +0600

    header sticky bg white added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ import MobileMenu from "./MobileMenu";
 const Header = () => {
 	const [isMobileMenuOpen,setIsMobileMenuOpen]=useState(false);
   return (
-    <div className="header-wrapper z-50 sticky top-0">
+    <div className="header-wrapper z-50 sticky top-0 bg-white">
 <DesktopHeader/>
       <div className="mobile-header bg-white lg:hidden flex flex-row items-center justify-between px-5 py-4">
         <div className="col-left">
