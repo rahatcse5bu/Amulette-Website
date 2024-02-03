f9d22ff983eb88772c59ccb5cd2de64abe6c1f4a
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-02-03 22:48:37 +0600

    footer fixed

--- src/components/Footer/Copyright/Copyright.jsx
+++ src/components/Footer/Copyright/Copyright.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Copyright = () => {
   return (
-    <div className="copyright-wrapper mx-2  lg:mx-0">
-      <div className="copyright container bg-primaryColor rounded-md flex flex-col lg:flex-row items-center justify-between px-5 lg:px-10  pt-5 pb-4 lg:py-9 mb-[102px] lg:mb-auto mx-auto lg:mx-auto lg:mt-16">
+    <div className="copyright-wrapper mx-2  lg:mx-0 pb-10">
+      <div className="copyright container bg-primaryColor rounded-md flex flex-col lg:flex-row items-center justify-between px-5 lg:px-10  pt-5 pb-4 lg:py-9 mb--[102px] mx-auto lg:mx-auto lg:mt-16">
         <div className="copyright-text capitalize font-Montserrat text-xs lg:text-lg text-white text-center lg:text-left">
           © 2023 Amulette Limited. All rights reserved.
         </div>
