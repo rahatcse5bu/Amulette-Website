14f20a418ce2dda826d6cefba3817231fdc52269
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-02-03 22:47:16 +0600

    xcbvdfx

--- src/components/Footer/Copyright/Copyright.jsx
+++ src/components/Footer/Copyright/Copyright.jsx
@@ -1,19 +1,23 @@
-
-import React from 'react';
+import React from "react";
 
 const Copyright = () => {
-    return (
-        <div className='copyright-wrapper mx-2  lg:mx-0'>
-            <div className='copyright container bg-primaryColor rounded-md flex flex-col lg:flex-row items-center justify-between px-5 lg:px-10  pt-5 pb-4 lg:py-9 mb--[102px] mx-auto lg:mx-auto lg:mt-16'>
-<div className='copyright-text capitalize font-Montserrat text-xs lg:text-lg text-white text-center lg:text-left'>
-©  2023 Amulette Limited. All rights reserved.
-</div>
-<div className='copyright-logo mt-3 lg:mt-0'>
-<img src={'Images/Footer/Copyright-Logo.svg' } width={117} height={32} alt={'Amulette Logo'}/>
-</div>
-            </div>
+  return (
+    <div className="copyright-wrapper mx-2  lg:mx-0">
+      <div className="copyright container bg-primaryColor rounded-md flex flex-col lg:flex-row items-center justify-between px-5 lg:px-10  pt-5 pb-4 lg:py-9 mb-[102px] lg:mb-auto mx-auto lg:mx-auto lg:mt-16">
+        <div className="copyright-text capitalize font-Montserrat text-xs lg:text-lg text-white text-center lg:text-left">
+          © 2023 Amulette Limited. All rights reserved.
         </div>
-    );
+        <div className="copyright-logo mt-3 lg:mt-0">
+          <img
+            src={"Images/Footer/Copyright-Logo.svg"}
+            width={117}
+            height={32}
+            alt={"Amulette Logo"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Copyright;
