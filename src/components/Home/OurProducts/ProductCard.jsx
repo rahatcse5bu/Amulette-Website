82738245d46568b83fb0beb10bc0d4797b46b167
anichu <anis.cse5.bu@gmail.com>
2024-02-03 11:19:49 +0600

    add products

--- src/components/Home/OurProducts/ProductCard.jsx
+++ src/components/Home/OurProducts/ProductCard.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+/* eslint-disable react/prop-types */
 import { IoCartOutline } from "react-icons/io5";
 
-const ProductCard = ({product}) => {
-  return (
-    <div className="product-card flex flex-col items-center justify-center border border-black border-opacity-20 rounded-3xl py-6">
-      <div className="img-wrapper px-[30px] py-[15px] border border-[#F6F6F6] bg-[#F6F6F6] rounded-2xl">
-      <img src= {`${product.img}`} alt="" height={221} />
-      </div>
-      <h3 className="title text-[1C121F] text-sm lg:text-lg font-semibold font-Poppins">
-        {product.title}
-      </h3>
-      <p className="desc text-[9px] lg:text-xs text-[#1C121F] font-normal font-Poppins mt-2">
-      {product.description}
-      </p>
-      <div className="usd-add-to-cart flex flex-row items-center justify-center space-x-9 mt-3.5">
-        <span className="text-[#1C121F] text-sm  lg:text-lg font-semibold">${product.price}</span>
-        <button className="bg-primaryColor text-white px-8 lg:px-10 py-2.5 lg:py-3.5 rounded-[50px]">
-        <div className="flex flex-row items-center justify-center space-x-1.5">  <IoCartOutline size={16} /> <span>Add to cart</span></div>
-        </button>
-      </div>
-    </div>
-  );
+const ProductCard = ({ product, setShowModal }) => {
+	const showModalHandler = () => {
+		setShowModal((prev) => !prev);
+	};
+	return (
+		<div className="flex flex-col items-center justify-center py-6 border border-black product-card border-opacity-20 rounded-3xl">
+			<div
+				onClick={showModalHandler}
+				className="img-wrapper px-[30px] py-[15px] border border-[#F6F6F6] bg-[#F6F6F6] rounded-2xl"
+			>
+				<img src={`${product.img}`} alt="" height={221} />
+			</div>
+			<h3 className="title text-[1C121F] text-sm lg:text-lg font-semibold font-Poppins">
+				{product.title}
+			</h3>
+			<p className="desc text-[9px] lg:text-xs text-[#1C121F] font-normal font-Poppins mt-2">
+				{product.description}
+			</p>
+			<div className="usd-add-to-cart flex flex-row items-center justify-center space-x-9 mt-3.5">
+				<span className="text-[#1C121F] text-sm  lg:text-lg font-semibold">
+					${product.price}
+				</span>
+				<button className="bg-primaryColor text-white px-8 lg:px-10 py-2.5 lg:py-3.5 rounded-[50px]">
+					<div className="flex flex-row items-center justify-center space-x-1.5">
+						{" "}
+						<IoCartOutline size={16} /> <span>Add to cart</span>
+					</div>
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default ProductCard;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,10 +3,12 @@ import ProductCard from "../components/Home/OurProducts/ProductCard";
 import { productsData } from "../components/Home/OurProducts/ProductData";
 import { IoSearchSharp } from "react-icons/io5";
 import ProductNotFound from "../components/ProductNotFound";
-
+import { BsXCircle } from "react-icons/bs";
+import ProductDetailsPage from "./ProductDetailsPage";
 const ProductsPage = () => {
 	const [productSearchTitle, setProductSearchTitle] = useState("");
 	const [sortOption, setSortOption] = useState("");
+	const [showModal, setShowModal] = useState(false);
 	const filteredProducts = useMemo(() => {
 		let filtered = productsData.filter((product) =>
 			product.title.toLowerCase().includes(productSearchTitle.toLowerCase())
@@ -22,48 +24,71 @@ const ProductsPage = () => {
 	}, [productSearchTitle, sortOption]);
 
 	return (
-		<div className="product-page bg-[#FDFDFD] pt-7">
-			<div className="container px-5 mx-auto products-wrapper lg:px-0">
-				<h1 className="text-xl font-bold text-left  lg:text-2xl">
-					<span className="text-[#25282B]">Amulette</span>
-					<span className="text-primaryColor"> Accessories</span>
-				</h1>
-				<p className="text-[#25282B] text-[12px] lg:text-sm font-normal text-left mt-4">
-					Elevate Your Experience with Our Range of Stylish Accessories
-				</p>
-				<div className="flex flex-row items-center justify-between search-sort-wrapper space-x-7 mt-7">
-					<div className="search-wrapper w-3/4 lg:w-full flex flex-row items-center justify-center space-x-2.5 border border-[#DADADA] rounded-full px-2 lg:px-4 py-2 lg:py-3.5">
-						<IoSearchSharp className="text-primaryColor" />
-						<input
-							onChange={(e) => setProductSearchTitle(e.target.value)}
-							type="text"
-							value={productSearchTitle}
-							className="w-full"
-							placeholder="Search for product..."
-						/>
+		<>
+			<div className="product-page bg-[#FDFDFD] pt-7">
+				<div className="container px-5 mx-auto products-wrapper lg:px-0">
+					<h1 className="text-xl font-bold text-left lg:text-2xl">
+						<span className="text-[#25282B]">Amulette</span>
+						<span className="text-primaryColor"> Accessories</span>
+					</h1>
+					<p className="text-[#25282B] text-[12px] lg:text-sm font-normal text-left mt-4">
+						Elevate Your Experience with Our Range of Stylish Accessories
+					</p>
+					<div className="flex flex-row items-center justify-between search-sort-wrapper space-x-7 mt-7">
+						<div className="search-wrapper w-3/4 lg:w-full flex flex-row items-center justify-center space-x-2.5 border border-[#DADADA] rounded-full px-2 lg:px-4 py-2 lg:py-3.5">
+							<IoSearchSharp className="text-primaryColor" />
+							<input
+								onChange={(e) => setProductSearchTitle(e.target.value)}
+								type="text"
+								value={productSearchTitle}
+								className="w-full"
+								placeholder="Search for product..."
+							/>
+						</div>
+						<select
+							value={sortOption}
+							onChange={(e) => setSortOption(e.target.value)}
+							className="w-1/4 lg:w-auto border border-primaryColor px-2 lg:px-5 py-2 lg:py-2.5  rounded-full"
+						>
+							<option disabled value="Sort by">
+								Sort By: Price low to high
+							</option>
+							<option value="low-to-high">Price: Low to High</option>
+							<option value="high-to-low">Price: High to Low</option>
+						</select>
 					</div>
-					<select
-						value={sortOption}
-						onChange={(e) => setSortOption(e.target.value)}
-						className="w-1/4 lg:w-auto border border-primaryColor px-2 lg:px-5 py-2 lg:py-2.5  rounded-full"
-					>
-						<option disabled value="Sort by">
-							Sort By: Price low to high
-						</option>
-						<option value="low-to-high">Price: Low to High</option>
-						<option value="high-to-low">Price: High to Low</option>
-					</select>
+					<div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-4 mt-9">
+						{filteredProducts.length > 0 &&
+							filteredProducts?.map((data, index) => (
+								<ProductCard
+									setShowModal={setShowModal}
+									key={index}
+									product={data}
+									index={index}
+								/>
+							))}
+					</div>
 				</div>
-				<div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-4 mt-9">
-					{filteredProducts.length > 0 &&
-						filteredProducts?.map((data, index) => (
-							<ProductCard key={index} product={data} index={index} />
-						))}
-				</div>
+
+				{filteredProducts.length <= 0 && <ProductNotFound />}
 			</div>
 
-			{filteredProducts.length <= 0 && <ProductNotFound />}
-		</div>
+			{showModal && (
+				<div className="fixed inset-0 bg-[rgba(0,0,0,0.3)] z-[99999] flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+					<div className="relative w-[90%] h-[90vh] overflow-y-auto bg-white z-[9999] my-6 rounded-md mx-auto">
+						<div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+							<div className="absolute top-2 right-2 ">
+								<BsXCircle
+									onClick={() => setShowModal(false)}
+									className="w-5 h-5 text-red-600 cursor-pointer"
+								/>
+							</div>
+						</div>
+						<ProductDetailsPage />
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 export default ProductsPage;
