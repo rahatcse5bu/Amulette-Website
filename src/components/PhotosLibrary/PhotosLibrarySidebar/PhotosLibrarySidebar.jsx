2dd4fa571d9298221790ceabb923789815957469
anichu <anis.cse5.bu@gmail.com>
2024-02-03 07:49:12 +0600

    add amation

--- src/components/PhotosLibrary/PhotosLibrarySidebar/PhotosLibrarySidebar.jsx
+++ src/components/PhotosLibrary/PhotosLibrarySidebar/PhotosLibrarySidebar.jsx
@@ -2,10 +2,19 @@
 import License from "../License/License";
 import Category from "../Category/Category";
 import { HiMiniXCircle } from "react-icons/hi2";
+import classNames from "classnames";
 
-const PhotosLibrarySidebar = ({ sideBarHandler }) => {
+const PhotosLibrarySidebar = ({ sideBarHandler, sideBarOpen }) => {
 	return (
-		<div className="col-left p-2 rounded-3xl absolute top-0 border bg-white z-50 w-full border-[#979898]">
+		<div
+			className={classNames(
+				"col-left p-2 rounded-xl absolute top-0 border bg-white z-50 w-full border-[#979898]",
+				{
+					"active-sidebar-open": sideBarOpen === true,
+					"inactive-sidebar-open": sideBarOpen !== true,
+				}
+			)}
+		>
 			<div
 				className="text-left flex justify-end z-[2000] cursor-pointer"
 				onClick={sideBarHandler}
