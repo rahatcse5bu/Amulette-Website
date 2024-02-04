ba0fbee01554361a18e80296e034265091259abb
anichu <anis.cse5.bu@gmail.com>
2024-02-04 18:37:21 +0600

    change select language button

--- src/components/Header/DesktopHeader.jsx
+++ src/components/Header/DesktopHeader.jsx
@@ -1,49 +1,66 @@
 import { NavLink } from "react-router-dom";
 import { menuItems } from "../../constants/constant-data";
+import { useState } from "react";
 
 const DesktopHeader = () => {
-  return (
-    <div className="containerr max-w-amulette lg:px-amulette hidden lg:flex flex-row items-center justify-between py-6 mx-auto header">
-      <div className="logo">
-        <img src="Images/Header/Logo.svg" alt="amulette logo" />
-      </div>
-      <div className="header-menu-items">
-        <ul className="flex flex-row items-center justify-center space-x-10 text-lg font-semibold text-menuColor font-Poppins">
-          {menuItems.map((item) => {
-            return (
-              <li key={item.id} className="">
-                <NavLink
-                  className={({ isActive }) =>
-                    isActive
-                      ? "active-header relative text-primaryColor"
-                      : "inactive-header"
-                  }
-                  to={item.path}
-                >
-                  {item.name}
-                  <span className=" bottom-[-4px]   w-[20px] mx-auto h-[3px] bg-primaryColor"></span>
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="flex flex-row items-center justify-center space-x-10 cart-lang">
-        <div className="flex flex-row items-center justify-center cart-wrapper">
-          {/* <span className="mr-1 text-lg font-semibold text-primaryColor font-Poppins">
+	const [lang, setLang] = useState("english");
+	return (
+		<div className="containerr max-w-amulette lg:px-amulette hidden lg:flex flex-row items-center justify-between py-6 mx-auto header">
+			<div className="logo">
+				<img src="Images/Header/Logo.svg" alt="amulette logo" />
+			</div>
+			<div className="header-menu-items">
+				<ul className="flex flex-row items-center justify-center space-x-10 text-lg font-semibold text-menuColor font-Poppins">
+					{menuItems.map((item) => {
+						return (
+							<li key={item.id} className="">
+								<NavLink
+									className={({ isActive }) =>
+										isActive
+											? "active-header relative text-primaryColor"
+											: "inactive-header"
+									}
+									to={item.path}
+								>
+									{item.name}
+									<span className=" bottom-[-4px]   w-[20px] mx-auto h-[3px] bg-primaryColor"></span>
+								</NavLink>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="flex flex-row items-center justify-center space-x-10 cart-lang">
+				<div className="flex flex-row items-center justify-center cart-wrapper">
+					{/* <span className="mr-1 text-lg font-semibold text-primaryColor font-Poppins">
 							Cart
 						</span>
 						<img src="Images/Header/Cart.svg" /> */}
-        </div>
-        <select className="lang border border-primaryColor rounded-[54px] py-2 px-4">
-          <option selected value="english">
-            Eng
-          </option>
-          <option value="bangla">Bn</option>
-        </select>
-      </div>
-    </div>
-  );
+				</div>
+				<div className="border flex items-center px-2 border-primaryColor py-2 rounded-3xl ">
+					<img
+						src={
+							lang === "english"
+								? "../../../public/Images/Header/us.png"
+								: "../../../public/Images/Header/bd.png"
+						}
+						className="w-6 h-6 rounded-full"
+						alt=""
+					/>
+					<select
+						value={lang}
+						onChange={(e) => setLang(e.target.value)}
+						className="lang outline-none px-2 rounded-3xl"
+					>
+						<option selected className="m-2" value="english">
+							Eng
+						</option>
+						<option value="bangla">Bn</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DesktopHeader;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,48 +8,69 @@ import { useState } from "react";
 import MobileMenu from "./MobileMenu";
 
 const Header = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  return (
-    <div className="header-wrapper z-50 sticky top-0 left-0 bg-white shadow-md">
-      <DesktopHeader />
-      <div className="mobile-header bg-white lg:hidden flex flex-row items-center justify-between px-5 py-4 shadow-md">
-        <div className="col-left">
-          {isMobileMenuOpen && (
-            <IoCloseSharp
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              size={26}
-            />
-          )}
-          {!isMobileMenuOpen && (
-            <RiMenu5Fill
-              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              size={26}
-            />
-          )}
-        </div>
-        <div className="col-middle menu-items">
-          <img src="Images/Header/Logo.svg" />
-        </div>
-        <div className="col-right">
-          <div className="flex flex-row items-center justify-center cart-lang">
-            <div className="flex flex-row items-center justify-center cart-wrapper">
-              {/* <span className="mr-1 text-lg font-semibold text-primaryColor font-Poppins">
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const [lang, setLang] = useState("english");
+	return (
+		<div className="header-wrapper z-50 sticky top-0 left-0 bg-white shadow-md">
+			<DesktopHeader />
+			<div className="mobile-header bg-white lg:hidden flex flex-row items-center justify-between px-5 py-4 shadow-md">
+				<div className="col-left">
+					{isMobileMenuOpen && (
+						<IoCloseSharp
+							onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+							size={26}
+						/>
+					)}
+					{!isMobileMenuOpen && (
+						<RiMenu5Fill
+							onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+							size={26}
+						/>
+					)}
+				</div>
+				<div className="col-middle menu-items">
+					<img src="Images/Header/Logo.svg" />
+				</div>
+				<div className="col-right">
+					<div className="flex flex-row items-center justify-center cart-lang">
+						<div className="flex flex-row items-center justify-center cart-wrapper">
+							{/* <span className="mr-1 text-lg font-semibold text-primaryColor font-Poppins">
 							Cart
 						</span>
 						<img src="Images/Header/Cart.svg" /> */}
-            </div>
-            <select className="lang border border-primaryColor rounded-[54px] py-2 px-4">
-              <option selected value="english">
-                Eng
-              </option>
-              <option value="bangla">Bn</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      {isMobileMenuOpen && <MobileMenu isMobileMenuOpen={isMobileMenuOpen} setIsMobileMenuOpen={setIsMobileMenuOpen}/>}
-    </div>
-  );
+						</div>
+						<div className="border flex items-center px-2 border-primaryColor py-2 rounded-3xl ">
+							<img
+								src={
+									lang === "english"
+										? "../../../public/Images/Header/us.png"
+										: "../../../public/Images/Header/bd.png"
+								}
+								className="w-6 h-6 rounded-full"
+								alt=""
+							/>
+							<select
+								value={lang}
+								onChange={(e) => setLang(e.target.value)}
+								className="lang outline-none px-2 rounded-3xl"
+							>
+								<option selected className="m-2" value="english">
+									Eng
+								</option>
+								<option value="bangla">Bn</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>
+			{isMobileMenuOpen && (
+				<MobileMenu
+					isMobileMenuOpen={isMobileMenuOpen}
+					setIsMobileMenuOpen={setIsMobileMenuOpen}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default Header;
